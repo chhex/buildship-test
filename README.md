300ca70cf03870b0fecad02ee071c3e04e2bf948
Christoph Henrici <chhenrici@gmail.com>
2018-02-13 22:26:34 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,28 @@
+Buildship Test Szenario: 
+
+Preconditions: 
+
+A Empty local Maven Repo, before 
+
+Steps: 
+
+1. First import test-m2e into Eclipse 
+2. Then  import test-gradle into Eclipse 
+
+Expectation:
+
+When navigating /test-gradle/src/main/java/TestCli.java to the declaration example org.apache.commons.cli.Options to see the source code, which isn't the case.
+
+Only after doing a Maven Update of /test-m2e the source code is downloaded.
+
+Enviroment: 
+
+Eclipse Oxygen.2 Release (4.7.2)
+
+m2e 1.8.2.20171007-021
+
+Buildship 2.2.1.v20180125-1441
+
+Gradle 4.3.1
+
+Note: I had the same szeanrio in other Enviroments
---
+++ test-m2e/src/main/java/TestCli.java
@@ -0,0 +1,31 @@
+
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class TestCli {
+
+	public static void main(String[] args) throws ParseException {
+		Options options = new Options();
+		options.addOption("t", false, "display current time");
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+		Date date = new Date();
+		if (cmd.hasOption("t")) {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			System.out.println(dateFormat.format(date));
+		} else {
+			DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+			System.out.println(dateFormat.format(date));
+		}
+
+	}
+
+}
